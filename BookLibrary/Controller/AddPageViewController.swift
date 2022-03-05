c91cfaab24b1e4c3457ac4fe7c30c3ea3fe09c5e
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-03-06 00:33:52 +0300

    addViewController is completed

--- BookLibrary/Controller/AddPageViewController.swift
+++ BookLibrary/Controller/AddPageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddPageViewController: UIViewController {
+class AddPageViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var bookImageView: UIImageView!
     @IBOutlet weak var bookNameInput: UITextField!
@@ -15,15 +15,49 @@ class AddPageViewController: UIViewController {
     @IBOutlet weak var PageInput: UITextField!
     @IBOutlet weak var SaveButton: UIButton!
     
+    var bookName : String?
+    var authorName : String?
+    var pageNumber : Int?
     
+    let bookData = BookData()
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let recognizerGesture = UITapGestureRecognizer.init(target: self, action: #selector(selectImage))
+        bookImageView.addGestureRecognizer(recognizerGesture)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        if bookNameInput.text != "" {
+           bookName = bookNameInput.text
+        }
+        if(AuthorInput.text != ""){
+             authorName = AuthorInput.text
+        }
+        if(PageInput.text != ""){
+            pageNumber = Int(PageInput.text!)
+        }
+        let imageData = bookImageView.image?.jpegData(compressionQuality: 0.5)
+        
+        let book = Book(bookName: bookName!, bookAuthor: authorName!, bookID: UUID(), bookImage: imageData!, bookPageNumber: pageNumber!)
+        bookData.addToDatabase(book: book)
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
     }
+    
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    
     
    
 }
--- BookLibrary/Controller/ViewController.swift
+++ BookLibrary/Controller/ViewController.swift
@@ -7,15 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+   
+    
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-
+    @objc func addButtonClicked() {
+        
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    
 }
 
