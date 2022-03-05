7ade4093e289d11b50feec07e5d2e4f1a0649c52
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-03-06 01:05:19 +0300

    ViewController Add book side completed

--- BookLibrary/Controller/ViewController.swift
+++ BookLibrary/Controller/ViewController.swift
@@ -12,34 +12,54 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
     
 
     @IBOutlet weak var tableView: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    var bookData = BookData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        getData()
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add,target: self, action: #selector(addButtonClicked))
         tableView.delegate = self
         tableView.dataSource = self
     }
 
     @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toAddBookPage", sender: nil)
+    }
+    
+    @objc func getData(){
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        let bookList = bookData.getBooks()
+        if bookList.count > 0 {
+            for book in bookList {
+                nameArray.append(book.name!)
+                idArray.append(book.id!)
+                
+        }
+            self.tableView.reloadData()
+        }
         
     }
     
     
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return nameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = nameArray[indexPath.row]
         return cell
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newData"), object: nil)
+    }
     
 }
 
